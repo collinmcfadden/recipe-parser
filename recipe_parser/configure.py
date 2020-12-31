79d3b3594c8b9c2b80d93e2020859e0af6f6f7fa
collinmcfadden <cmm@brunkhorst.org>
2020-12-30 20:04:44 -0600

    config file for collection groupings instead of user input

---
+++ recipe_parser/configure.py
@@ -0,0 +1,16 @@
+
+
+BACKUP_COLLECTIONS = {
+	"Drinks": ["Fruit Salads"],
+	"Salsas & Spreads": ["Dips"],
+	"Biscuits & Rolls": ["Breads"],
+	"Muffins": ["Biscuits & Rolls"],
+	"Frosting": ["Cakes"],
+	"Main Dishes - Pork": ["Main Dishes - Ham", "Main Dishes - Beef"],
+	"Main Dishes - Vegetarian": ["Main Dishes - Seafood", "Salads"],
+	"Main Dishes - Turkey": ["Main Dishes - Chicken"],
+	"Main Dishes - Ham": ["Main Dishes - Pork", "Main Dishes - Beef"],
+	"Main Dishes - Seafood": ["Main Dishes - Vegetarian", "Salads"],
+	"Sandwiches": ["Soups"],
+	"Miscellaneous": ["Canning & Freezing"]
+}
--- recipe_parser/recipe_parser.py
+++ recipe_parser/recipe_parser.py
@@ -4,6 +4,7 @@ import sys
 import string
 from recipe import Recipe
 from collection import Collection
+import configure
 
 import random
 random.seed(0)
@@ -21,7 +22,7 @@ current_recipe_raw_lines = []
 
 current_collection = None
 collection_id = 100
-collection_list = []
+collection_dict = dict()
 
 # Create the list of raw recipe details
 for line in input_file:
@@ -35,7 +36,7 @@ for line in input_file:
 		# Ex: -APPETIZERS AND BEVERAGES
 		collection_str = string.capwords(line.strip("-"))
 		current_collection = Collection(collection_id, collection_str)
-		collection_list.append(current_collection)
+		collection_dict[collection_str] = current_collection
 		collection_id += 1
 	else: 
 		# Append all other lines
@@ -53,36 +54,41 @@ for raw_recipe_list, collection in raw_recipe_list:
 		recipe_id += 1
 
 # Now that the recipes have been parsed and added to Collections, cycle through each collection
-# If a collection has less than 10 members, prompt user input to ask what other collections of 
-# recipes should be suggested by its recipes
-# Ex. "Main Dish - Turkey" collection has only one member, so "Main Dish - Chicken" recipes should be used as additional recipe suggestions
-print(", ".join([f"{index}: {collection.name}" for index, collection in enumerate(collection_list)]))
+# If a collection has less than 10 members, check the configure.py file to see what the backup collections
+# we should use to fill in the remaining recipe suggestions. If there are not enough recipes to satisfy the 
+# requirement, request the user updates the configure.py file
+# Ex. "Main Dish - Turkey" collection has only one member, so "Main Dish - Chicken" recipes should be used for additional recipe suggestions
+print("\n".join([f"{collection.name} contains {len(collection.recipe_list)} recipes" for collection in collection_dict.values()]))
 
-for collection in collection_list:
+for collection_name, collection in collection_dict.items():
 	num_recipes = len(collection.recipe_list)
 
 	# We want 10 suggestions per recipe, so need to ensure there are 11 available in the collections (because a recipe can't recommend itself - that would be narcissistic)
 	while num_recipes < 11:
-		additional_collection_index = input((f"Need {11 - num_recipes} more recipes to suggest to {collection.name}. "
-											 f"Enter the index of the collection that should be used for additional recommendations."))
-		# Ensure user input data quality
-		if additional_collection_index.isdigit() and not 0 <= int(additional_collection_index) < len(collection_list):
-			print(f"Input of {additional_collection_index} is invalid. Must be integer from 0-{len(collection_list) - 1}.")
+		if collection_name not in configure.BACKUP_COLLECTIONS:
+			raise ValueError(f"{collection_name} only has {num_recipes} recipes but has no backup collections specified in configure.py")
 		else:
-			backup_collection = collection_list[int(additional_collection_index)]
-			collection.backup_collections.append(backup_collection)
-			num_recipes += len(backup_collection.recipe_list)
-
+			try:
+				backup_name = configure.BACKUP_COLLECTIONS[collection_name].pop(0)
+			except IndexError:
+				raise ValueError(f"Collection {collection_name} failed to specify more than 10 recipes. Gave {num_recipes} including those from {collection.backup_collections}")
+			
+			if backup_name not in collection_dict:
+				raise ValueError(f"{collection_name} in configure.py specified {backup_name} as a backup, but it does not exist")
+			else:
+				backup_collection = collection_dict[backup_name]
+				collection.backup_collections.append(backup_collection)
+				num_recipes += len(backup_collection.recipe_list)
+		
 # Add the Dynamo JSON details to our running list of dynamo records to upload
 dynamo_records = list()
 
 # For each collection, add the records to the dynamo list
-for collection in collection_list:
+for collection in collection_dict.values():
 	dynamo_records.append(collection.get_dynamo_dictionary())
 
 # For each recipe, randomly generate recipe suggestion from recipes in its category/backup categories
 for recipe in recipe_class_list:
-	print(recipe.title)
 	dynamo_records.extend(recipe.get_dynamo_dictionaries())
 	dynamo_records.extend(recipe.collection.get_recipe_suggestions(recipe))
 
